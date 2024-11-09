825f19cacccd2aff77bd73c08c0f6d9cc75a4c34
Student <mpodzielny18@gmail.com>
2024-11-09 09:22:02 +0100

    luks

--- src/cwicz.java
+++ src/cwicz.java
@@ -1,8 +1,45 @@
+// od 100 zl do 10 tys zl
+// 6-12 rat = 2,5%
+// 13-24 rat = 5%
+// 25-48 rat = 10%
+
+import java.util.Scanner;
 public class cwicz {
     public static void main(String[] args) {
-        int najwieksza;
-        int najmniejsza;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Podaj cene towaru: ");
+        int price = input.nextInt();
+        input.nextLine();
+        System.out.print("Podaj ilość  rat: ");
+        int rate = input.nextInt();
+        input.nextLine();
+        input.close();
+        System.out.println("-------------------------");
+
+        if (price < 100 || price > 10_000 || rate < 6 || rate > 48) {
+            System.out.println("Podano błędne dane !!!:");
+            System.out.println("Cena powinna być w przedziale od 100zł do 10 000zł");
+            System.out.println("Liczba rat powinna być w przedziale od 6 do 48");
+        } else {
+                double mnoznik;
+                if (rate <= 12) {
+                    mnoznik = 0.025;
+                } else if (rate <= 24) {
+                    mnoznik = 0.05;
+                } else {
+                    mnoznik = 0.1;
+                }
+
+                double odsetki = (price * mnoznik);
+                double TotalPrize = (price + odsetki);
+                double OneRate = (odsetki / rate);
+                System.out.println("Kwota odsetek wynosi: " + odsetki + "zł");
+                System.out.println("Suma rat wynosi: " + TotalPrize + "zł");
+                System.out.println("Kwota jednej raty wynosi: " + OneRate + "zł");
 
 
+            }
+        }
     }
-}
+
